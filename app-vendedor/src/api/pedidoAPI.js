25cb6d78e2084b2d49e82eb29538f61f40632b49
Arthur Bressan <arthurbressan2@hotmail.com>
2020-02-15 14:59:19 -0300

    Pedidospage listign

--- app-vendedor/src/api/pedidoAPI.js
+++ app-vendedor/src/api/pedidoAPI.js
@@ -1,11 +1,22 @@
+function uuidv4() {
+  // Crappy uuidv4 generator for testing purposes
+  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+    var r = (Math.random() * 16) | 0,
+      v = c === 'x' ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
+
 const PedidoAPI = {
   create: (product, user, data) => {
     return new Promise((resolve, reject) => {
       resolve({
-        eid: 'ABCDEF',
+        eid: uuidv4(),
         product: product.id,
         user: user.eid,
         data: data,
+        status: 'NEW',
+        created_date: new Date().toISOString(),
       });
     });
   },
--- app-vendedor/src/components/PedidosPage/PedidosPage.js
+++ app-vendedor/src/components/PedidosPage/PedidosPage.js
@@ -4,10 +4,33 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { Page } from '../common/Page';
 
-export class PedidoPage extends Component {
+export class PedidosPage extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pedidos: this.flattenAndOrderPedidos(props.pedidos),
+    };
+  }
+
+  flattenAndOrderPedidos = (pedidosMap) => {
+    const entries = pedidosMap.entries;
+    if (!entries) {
+      return [];
+    }
+    const pedidos = pedidosMap.entries.map(([eid, pedido]) => pedido);
+    return pedidos.sort((a, b) => {
+      if (a.created_date < b.created_date) {
+        return -1;
+      }
+      return 1;
+    });
+  };
+
   static propTypes = {
     user: PropTypes.object.isRequired,
     location: PropTypes.object.isRequired,
+    pedidos: PropTypes.object.isRequired,
   };
 
   render() {
@@ -17,7 +40,11 @@ export class PedidoPage extends Component {
         user={this.props.user}
         location={this.props.location}
       >
-        <div>PedidoPage</div>
+        {this.state.pedidos.length ? (
+          <div></div>
+        ) : (
+          <div>Sem pedidos para mostrar</div>
+        )}
       </Page>
     );
   }
@@ -25,8 +52,9 @@ export class PedidoPage extends Component {
 
 const mapStateToProps = (state) => ({
   user: state.account.user,
+  pedidos: state.pedido.pedidos,
 });
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(PedidoPage);
+export default connect(mapStateToProps, mapDispatchToProps)(PedidosPage);
