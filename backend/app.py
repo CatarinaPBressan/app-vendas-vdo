f7c46879b3f0362253ecbd50c32353d03ffa65ed
Arthur Bressan <arthurbressan2@hotmail.com>
2020-02-20 15:52:58 -0300

    app_vendedor -> backoffice

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,11 @@
-from app_vendedor import create_app
+from backoffice import create_app
 
 app = create_app()
 
 
 @app.shell_context_processor
 def make_shell_context():
-    from app_vendedor.models import db, Usuario, Token, Pedido, PedidoProduto
+    from backoffice.models import db, Usuario, Token, Pedido, PedidoProduto
 
     return {
         "db": db,
--- backend/backoffice/__init__.py
+++ backend/backoffice/__init__.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
-from app_vendedor import (
+from backoffice import (
     settings,
     base,
     routes,
