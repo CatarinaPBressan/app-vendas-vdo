e4c1ea06a317248cc5b7f995562044239dd89ee6
Arthur de Paula Bressan <arthurbressan2@hotmail.com>
2020-02-20 20:09:05 +0000

    Resolve "API pedidos"

--- backend/backoffice/api/v0/__init__.py
+++ backend/backoffice/api/v0/__init__.py
@@ -2,11 +2,16 @@ from flask import Blueprint
 
 from flask_restful import Api
 
-from backoffice.api.v0.users import LoginAPI
+from backoffice.api.v0.usuarios import LoginAPI
+from backoffice.api.v0.pedidos import PedidosAPI, PedidoAPI
 
 
 def init_app(app, api_template):
     api_v0 = Blueprint("api_v0", __name__)
     api = Api(api_v0)
-    api.add_resource(LoginAPI, api_template, "/users/")
+
+    api.add_resource(LoginAPI, api_template, "/usuarios/")
+    api.add_resource(PedidosAPI, api_template, "/pedidos/")
+    api.add_resource(PedidoAPI, api_template, "/pedidos/<string:pedido_eid>/")
+
     app.register_blueprint(api_v0, url_prefix=api_template.format(version="v0"))
--- backend/backoffice/auth.py
+++ backend/backoffice/auth.py
@@ -1,6 +1,6 @@
 from flask import g
 
-from backoffice.base import basic_auth
+from backoffice.base import basic_auth, token_auth
 from backoffice.models import Usuario
 
 
@@ -8,6 +8,15 @@ from backoffice.models import Usuario
 def verify_password(username, password):
     usuario = Usuario.query.filter_by(username=username).first()
     if not usuario or not usuario.verify_password(password):
+        return False
+    g.usuario = usuario
+    return True
+
+
+@token_auth.verify_token
+def verify_token(token):
+    usuario = Usuario.get_user_via_token(token)
+    if not usuario:
         return False
     g.usuario = usuario
     return True
