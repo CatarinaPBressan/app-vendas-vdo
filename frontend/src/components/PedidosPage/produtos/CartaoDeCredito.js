f4ed16e9c1c5e65b89e3e9335ea4703da344ffbf
Arthur Bressan <arthurbressan2@hotmail.com>
2021-02-27 10:08:37 -0300

    Adiciona campo de data nascimento no cartão de crédito
    * Adiciona mascara de CEP no CEP pernoite (Seguro Automotivo)
    * Adiciona mapa opcao -> valores no CC

--- frontend/src/components/PedidosPage/produtos/CartaoDeCredito.js
+++ frontend/src/components/PedidosPage/produtos/CartaoDeCredito.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Card, Form, Col } from "react-bootstrap";
 
-const CartaoDeCredito = (props) => {
+const CartaoDeCredito = ({ data }) => {
   return (
     <Card>
       <Card.Header>Cartão de Crédito</Card.Header>
@@ -9,59 +9,92 @@ const CartaoDeCredito = (props) => {
         <Form.Row>
           <Form.Group controlId="cep" as={Col} lg={4}>
             <Form.Label>CEP</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.cep} />
+            <Form.Control plaintext readOnly value={data.cep} />
           </Form.Group>
           <Form.Group controlId="uf" as={Col} lg={4}>
             <Form.Label>UF</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.uf} />
+            <Form.Control plaintext readOnly value={data.uf} />
           </Form.Group>
           <Form.Group controlId="cidade" as={Col} lg={4}>
             <Form.Label>Cidade</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.cidade} />
+            <Form.Control plaintext readOnly value={data.cidade} />
           </Form.Group>
         </Form.Row>
         <Form.Row>
           <Form.Group controlId="logradouro" as={Col}>
             <Form.Label>Logradouro</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.logradouro} />
+            <Form.Control plaintext readOnly value={data.logradouro} />
           </Form.Group>
         </Form.Row>
         <Form.Row>
           <Form.Group controlId="endereco_numero" as={Col} lg={3}>
             <Form.Label>Número</Form.Label>
-            <Form.Control
-              plaintext
-              readOnly
-              value={props.data.endereco_numero}
-            />
+            <Form.Control plaintext readOnly value={data.endereco_numero} />
           </Form.Group>
           <Form.Group controlId="complemento" as={Col} lg={9}>
             <Form.Label>Complemento</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.complemento} />
+            <Form.Control plaintext readOnly value={data.complemento} />
           </Form.Group>
         </Form.Row>
         <Form.Row>
-          <Form.Group controlId="estado_civil" as={Col} lg={4}>
+          <Form.Group controlId="estado_civil" as={Col} lg={6}>
             <Form.Label>Estado Civil</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.estado_civil} />
+            <Form.Control
+              plaintext
+              readOnly
+              value={
+                {
+                  solteiro: "Solteiro",
+                  casado: "Casado(a) ou União Estável",
+                  viuvo: "Viúvo(a)",
+                  separado: "Separado(a) ou Divorciado(a)",
+                }[data.estado_civil]
+              }
+            />
           </Form.Group>
-          <Form.Group controlId="ocupacao" as={Col} lg={4}>
+          <Form.Group controlId="data_nascimento" as={Col} lg={6}>
+            <Form.Label>Data Nascimento</Form.Label>
+            <Form.Control plaintext readOnly value={data.data_nascimento} />
+          </Form.Group>
+        </Form.Row>
+        <Form.Row>
+          <Form.Group controlId="ocupacao" as={Col} lg={6}>
             <Form.Label>Ocupação</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.ocupacao} />
+            <Form.Control
+              plaintext
+              readOnly
+              value={
+                {
+                  assalariado: "Assalariado",
+                  empresario: "Empresario",
+                  aposentado: "Aposentado ou Pensionista",
+                  autonomo: "Autônomo",
+                  outros: "Outros",
+                }[data.ocupacao]
+              }
+            />
           </Form.Group>
-          <Form.Group controlId="data_vencimento" as={Col} lg={4}>
+          <Form.Group controlId="data_vencimento" as={Col} lg={6}>
             <Form.Label>Data de Vencimento</Form.Label>
             <Form.Control
               plaintext
               readOnly
-              value={props.data.data_vencimento}
+              value={
+                {
+                  dia_5: "5",
+                  dia_10: "10",
+                  dia_15: "15",
+                  dia_20: "20",
+                  dia_25: "25",
+                }[data.data_vencimento]
+              }
             />
           </Form.Group>
         </Form.Row>
         <Form.Row>
           <Form.Group controlId="nome_mae" as={Col}>
             <Form.Label>Nome da Mãe</Form.Label>
-            <Form.Control plaintext readOnly value={props.data.nome_mae} />
+            <Form.Control plaintext readOnly value={data.nome_mae} />
           </Form.Group>
         </Form.Row>
       </Card.Body>
--- frontend/src/components/ProdutoPage/produtos/CartaoDeCredito.js
+++ frontend/src/components/ProdutoPage/produtos/CartaoDeCredito.js
@@ -7,90 +7,112 @@ import { FIELDS, UFS_BRASIL } from "../../../constants/fields";
 
 const CartaoDeCredito = () => {
   return (
-    <Card>
-      <Card.Header>Cartão de Crédito</Card.Header>
-      <Card.Body>
-        <Form.Group controlId="cep">
-          <Form.Label>CEP</Form.Label>
-          <InputMask mask={FIELDS.cep.mask}>
-            {() => (
-              <Form.Control
-                type="text"
-                placeholder={FIELDS.cep.placeholder}
-                pattern={FIELDS.cep.pattern}
-                inputMode="numeric"
-                name="cep"
-                required
-              />
-            )}
-          </InputMask>
-          <Form.Text className="text-muted">Somente números</Form.Text>
-        </Form.Group>
-        <Form.Group controlId="uf">
-          <Form.Label>UF</Form.Label>
-          <Form.Control as="select" name="uf" required>
-            <option value="">(Selecione)</option>
-            {UFS_BRASIL.map((uf) => (
-              <option value={uf} key={uf}>
-                {uf}
-              </option>
-            ))}
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="cidade">
-          <Form.Label>Cidade</Form.Label>
-          <Form.Control type="text" name="cidade" required />
-        </Form.Group>
-        <Form.Group controlId="logradouro">
-          <Form.Label>Logradouro</Form.Label>
-          <Form.Control type="text" name="logradouro" required />
-        </Form.Group>
-        <Form.Group controlId="endereco_numero">
-          <Form.Label>Número</Form.Label>
-          <Form.Control type="text" name="endereco_numero" required />
-        </Form.Group>
-        <Form.Group controlId="complemento">
-          <Form.Label>Complemento</Form.Label>
-          <Form.Control type="text" name="complemento" />
-        </Form.Group>
-        <Form.Group controlId="estado_civil">
-          <Form.Label>Estado Civil</Form.Label>
-          <Form.Control as="select" name="estado_civil" required>
-            <option value="">(Selecione)</option>
-            <option value="solteiro">Solteiro(a)</option>
-            <option value="casado">Casado(a) ou União Estável</option>
-            <option value="viuvo">Viúvo(a)</option>
-            <option value="separado">Separado(a) ou Divorciado(a)</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="ocupacao">
-          <Form.Label>Ocupação</Form.Label>
-          <Form.Control as="select" name="ocupacao" required>
-            <option value="">(Selecione)</option>
-            <option value="assalariado">Assalariado</option>
-            <option value="empresario">Empresario</option>
-            <option value="aposentado">Aposentado ou Pensionista</option>
-            <option value="autonomo">Autônomo</option>
-            <option value="outros">Outros</option>
-          </Form.Control>
-        </Form.Group>
-        <Form.Group controlId="nome_mae">
-          <Form.Label>Nome da Mãe</Form.Label>
-          <Form.Control type="text" name="nome_mae" required />
-        </Form.Group>
-        <Form.Group controlId="data_vencimento">
-          <Form.Label>Data de Vencimento</Form.Label>
-          <Form.Control as="select" name="data_vencimento" required>
-            <option value="">(Selecione)</option>
-            <option value="dia_5">5</option>
-            <option value="dia_10">10</option>
-            <option value="dia_15">15</option>
-            <option value="dia_20">20</option>
-            <option value="dia_25">25</option>
-          </Form.Control>
-        </Form.Group>
-      </Card.Body>
-    </Card>
+    <>
+      <Card>
+        <Card.Header>Cartão de Crédito - Endereço</Card.Header>
+        <Card.Body>
+          <Form.Group controlId="cep">
+            <Form.Label>CEP</Form.Label>
+            <InputMask mask={FIELDS.cep.mask}>
+              {() => (
+                <Form.Control
+                  type="text"
+                  placeholder={FIELDS.cep.placeholder}
+                  pattern={FIELDS.cep.pattern}
+                  inputMode="numeric"
+                  name="cep"
+                  required
+                />
+              )}
+            </InputMask>
+            <Form.Text className="text-muted">Somente números</Form.Text>
+          </Form.Group>
+          <Form.Group controlId="uf">
+            <Form.Label>UF</Form.Label>
+            <Form.Control as="select" name="uf" required>
+              <option value="">(Selecione)</option>
+              {UFS_BRASIL.map((uf) => (
+                <option value={uf} key={uf}>
+                  {uf}
+                </option>
+              ))}
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="cidade">
+            <Form.Label>Cidade</Form.Label>
+            <Form.Control type="text" name="cidade" required />
+          </Form.Group>
+          <Form.Group controlId="logradouro">
+            <Form.Label>Logradouro</Form.Label>
+            <Form.Control type="text" name="logradouro" required />
+          </Form.Group>
+          <Form.Group controlId="endereco_numero">
+            <Form.Label>Número</Form.Label>
+            <Form.Control type="text" name="endereco_numero" required />
+          </Form.Group>
+          <Form.Group controlId="complemento">
+            <Form.Label>Complemento</Form.Label>
+            <Form.Control type="text" name="complemento" />
+          </Form.Group>
+        </Card.Body>
+      </Card>
+      <Card>
+        <Card.Header>Cartão de Crédito - Dados Adicionais</Card.Header>
+        <Card.Body>
+          <Form.Group controlId="estado_civil">
+            <Form.Label>Estado Civil</Form.Label>
+            <Form.Control as="select" name="estado_civil" required>
+              <option value="">(Selecione)</option>
+              <option value="solteiro">Solteiro(a)</option>
+              <option value="casado">Casado(a) ou União Estável</option>
+              <option value="viuvo">Viúvo(a)</option>
+              <option value="separado">Separado(a) ou Divorciado(a)</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="ocupacao">
+            <Form.Label>Ocupação</Form.Label>
+            <Form.Control as="select" name="ocupacao" required>
+              <option value="">(Selecione)</option>
+              <option value="assalariado">Assalariado</option>
+              <option value="empresario">Empresario</option>
+              <option value="aposentado">Aposentado ou Pensionista</option>
+              <option value="autonomo">Autônomo</option>
+              <option value="outros">Outros</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="data_nascimento">
+            <Form.Label>Data Nascimento</Form.Label>
+            <InputMask mask={FIELDS.data.mask}>
+              {() => (
+                <Form.Control
+                  type="text"
+                  placeholder={FIELDS.data.placeholder}
+                  pattern={FIELDS.data.pattern}
+                  inputMode="numeric"
+                  name="data_nascimento"
+                  required
+                />
+              )}
+            </InputMask>
+          </Form.Group>
+          <Form.Group controlId="data_vencimento">
+            <Form.Label>Data de Vencimento</Form.Label>
+            <Form.Control as="select" name="data_vencimento" required>
+              <option value="">(Selecione)</option>
+              <option value="dia_5">5</option>
+              <option value="dia_10">10</option>
+              <option value="dia_15">15</option>
+              <option value="dia_20">20</option>
+              <option value="dia_25">25</option>
+            </Form.Control>
+          </Form.Group>
+          <Form.Group controlId="nome_mae">
+            <Form.Label>Nome da Mãe</Form.Label>
+            <Form.Control type="text" name="nome_mae" required />
+          </Form.Group>
+        </Card.Body>
+      </Card>
+    </>
   );
 };
 
